f1b32d62acb59d6cfd2e49388c53c127ab287313
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-08-22 00:08:19 +0530

    Inheritance and Access Modifiers

--- Inheritance.py
+++ Inheritance.py
@@ -15,3 +15,53 @@ e1.showDetails()
 e2 = Programmer("Ramesh", 404)
 e2.showDetails()
 e2.showLanguage()
+
+         #   ** Access Modifiers  **
+
+# Public
+class Student:
+    # constructor is defined
+    def __init__(self, age, name):
+        self.age = age        # public variable
+        self.name = name      # public variable
+
+obj = Student(10, "vinod")
+print(obj.age)
+print(obj.name)
+
+# Private
+class Student:
+    def __init__(self,age,name):
+        self.__age = age     # An Indication of private variable
+        self.__name = name   # An Indication of private variable
+
+obj = Student(11, 'Ramesh')
+# print(obj.__age)  # Cannot be access directly
+# print(obj.__name) # Cannot be access directly
+
+print(obj._Student__age)   # can be accessed indirectly
+print(obj._Student__name)  # can be accessed indirectly
+
+print(obj.__dir__())
+
+#Protected
+class Student:
+    def __init__(self):
+        self._name = 'billu'
+
+    def _funName(self):     # protected
+        return "billubhayanker"
+
+class Subject(Student):   # inheritance
+    pass
+
+obj = Student()
+obj1 = Subject()
+
+# calling by object of student class
+print(obj._name)
+print(obj._funName())
+
+# calling by object of student class
+print(obj1._name)
+print(obj1._funName())
