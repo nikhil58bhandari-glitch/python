9cafe881add50b4ccb1f5ac3f287a76a722c7c90
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-08-21 23:14:32 +0530

    Decorater, Getter, Setter

--- Decorators.py
+++ Decorators.py
@@ -1,19 +1,42 @@
-print("Decorators")
-
-def greet(fx):
- def mfx(*args, **kwags):
-    print("good Morning")
-    fx(*args, **kwags)
-    print("thanks for using this function")
- return mfx
-
+# print("Decorators")
+#
+# def greet(fx):
+#  def mfx(*args, **kwags):
+#     print("good Morning")
+#     fx(*args, **kwags)
+#     print("thanks for using this function")
+#  return mfx
+#
+# # @greet
+# def hello():
+#     print("hello world")
+#
 # @greet
-def hello():
-    print("hello world")
+# def add(a,b):
+#     print(a+b)
+#
+# greet(hello)()
+# add(7,5)
 
-@greet
-def add(a,b):
-    print(a+b)
 
-greet(hello)()
-add(7,5)
+   # ** Getters and Setters **
+
+class Myclass:
+    def __init__(self, value):
+        self._value = value
+
+    def show(self):
+        print(f"value is {self._value}")
+
+    @property     # getter
+    def ten_value(self):
+        return 10 * self._value
+
+    @ten_value.setter    # setter 
+    def ten_value(self, new_value):
+        self._value = new_value/10
+
+obj = Myclass(10)
+obj.ten_value = 69
+print(obj.ten_value)
+obj.show()
